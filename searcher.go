21b11833c0197fee8e60cc8b593fd67dee9c2a2f
benjamindimalanta <66483348+benjamindimalanta@users.noreply.github.com>
2025-06-12 15:52:11 +0400

    1

--- searcher.go
+++ searcher.go
@@ -27,16 +27,20 @@ type Searchable interface {
 }
 
 func initCrawler() {
-	c = colly.NewCollector(
+c = colly.NewCollector(
     colly.AllowedDomains(
         "natomanga.com",
-        "www.natomanga.com",       // ← This is critical
+        "www.natomanga.com",
         "readnatomanga.com",
         "www.readnatomanga.com",
     ),
+    colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "+
+        "AppleWebKit/537.36 (KHTML, like Gecko) "+
+        "Chrome/114.0.0.0 Safari/537.36"),
     colly.MaxDepth(2),
     colly.Async(true),
 )
+
 
 
 	extensions.RandomUserAgent(c)
